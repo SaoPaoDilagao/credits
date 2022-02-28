a8a3cfa3da76b3048194668ad0eea940fc7ac29a
Luigi Teglia <teglia.luigi@gmail.com>
2022-02-28 11:37:10 -0500

    Forgitten file to commit

--- src/main/java/com/nttdata/credits/service/CreditService.java
+++ src/main/java/com/nttdata/credits/service/CreditService.java
@@ -9,12 +9,10 @@ public interface CreditService {
 	
 	void createCredit(Credit credit);
 	Mono<Credit> findCreditById(String id);
-	//Mono<Credit> findCreditById(int id);
 	Flux<Credit> findCreditByClientFirstNameAndLastName(String firstName, String lastName);
 	Flux<Credit> findCreditByClientDocumentNumber(String documentNumber);
 	Mono<Credit> findCreditByNumber(String number);
 	Mono<Credit> update(Credit account);
 	Mono<Credit> delete(String id);
-	//Mono<Credit> delete(int id);
 
 }
