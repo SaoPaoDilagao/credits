a9eba0a12dff113fa7fdc1998e75efe21a68fdce
Luigi Teglia <teglia.luigi@gmail.com>
2022-03-04 06:35:34 -0500

    Add configuration for Eureka

--- src/main/java/com/nttdata/credits/config/WebFluxConfig.java
+++ src/main/java/com/nttdata/credits/config/WebFluxConfig.java
@@ -1,10 +1,12 @@
 package com.nttdata.credits.config;
 
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.config.EnableWebFlux;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
 
 @Configuration
 @EnableWebFlux
+@EnableEurekaClient
 public class WebFluxConfig  implements WebFluxConfigurer {
 }
