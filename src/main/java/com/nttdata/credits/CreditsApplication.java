b46f56003fc91ec6233916ea8d44e95e4df0c353
Luigi Teglia <teglia.luigi@gmail.com>
2022-03-04 10:34:26 -0500

    Remove Eureka Configuration

--- src/main/java/com/nttdata/credits/CreditsApplication.java
+++ src/main/java/com/nttdata/credits/CreditsApplication.java
@@ -3,6 +3,8 @@ package com.nttdata.credits;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+//@EnableEurekaClient
 @SpringBootApplication
 public class CreditsApplication {
 
