8f7d7220ee86836934f07334d798d7a2f1e82f9c
Jordi Diaz <jordi.diaz04@hotmail.com>
2022-03-21 23:26:37 -0500

    Update Azure CI/CD.

--- src/main/java/com/nttdata/credits/controller/CreditController.java
+++ src/main/java/com/nttdata/credits/controller/CreditController.java
@@ -5,6 +5,7 @@ import java.math.BigDecimal;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -30,6 +31,14 @@ import reactor.core.publisher.Mono;
 public class CreditController {
   @Autowired
   private CreditService creditService;
+
+  @Value("${application.config.description}")
+  private String description;
+
+  @GetMapping("/test")
+  public String test() {
+    return description;
+  }
 
   @GetMapping("/client/documentNumber/{documentNumber}")
   public Flux<Credit> findByClientDocumentNumber(@PathVariable String documentNumber) {
